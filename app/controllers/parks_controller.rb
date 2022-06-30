b0a800b98c88720dedf6c77f936450b74751ed15
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-06-30 16:50:40 -0700

    add CRUD methods to parks controller

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,7 +1,36 @@
 class ParksController < applicationController
 
-  private
+  def index 
+    @parks = Park.applicationController
+    json_response(@parks)
+  end
+
+  def show
+    @park = Park.find(params[:id])
+    json_response(@parks)
+  end
+
+  def create
+    @park = Park.create(park_params)
+    json_response(@park)
+  end
+
+  def update
+    @park = Park.find(params[:id])
+    @park.update(park_params)
+  end
+
+  def destroy
+    @park = Park.find(params[:id])
+    @park.destroy
+  end 
+  
+    private
   def json_response(object, status = :ok)
     render json: object, status: status
+  end
+
+  def park_params
+    params.permit(:name, :managed_by, :description)
   end
 end
