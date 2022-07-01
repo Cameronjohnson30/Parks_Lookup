15e909b98a7df73cc9abf1339517fc98548bb7b9
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-06-30 21:28:12 -0700

    add rescue froms, render not found, render unprocessable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
   include Response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def render_not_found_response(exception)
+    json_response({ message: exception.message }, :not_found)
+  end
+
+  def render_unprocessable_entity_response(exception)
+    json_response({ message: exception.message }, :unprocessable_entity)
+  end
 end
 
