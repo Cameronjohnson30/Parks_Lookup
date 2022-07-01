6ea72de4fbbce915d3c0d9b29a3f225930e95f26
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-07-01 00:31:31 -0700

    add testing for pagination

--- spec/requests/get_route_spec.rb
+++ spec/requests/get_route_spec.rb
@@ -9,6 +9,11 @@ describe "get all parks route", :type => :request do
     expect(JSON.parse(response.body).size).to eq(10)
   end
 
+  it 'returns 10 parks at a time' do
+    get '/parks?page=1'
+    expect(JSON.parse(response.body).size).to eq(10)
+  end
+
   it 'returns status code 200' do
     expect(response).to have_http_status(:success)
   end
